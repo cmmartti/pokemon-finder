d3ee7fe71c3be6f48bbd637ebf940fde8d623f35
Charles Marttinen <mail@charlesmarttinen.ca>
2019-03-22 18:33:20 -0400

    Remove unnecessary ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ErrorBoundary from './ErrorBoundary';
 import useAppState from './state/useAppState';
 import Header from './Header';
 import SearchSettings from './SearchSettings';
@@ -12,27 +11,25 @@ export default function App({history, isLoading, error}) {
     return (
         <>
             <Header state={state} dispatch={dispatch} />
-            <ErrorBoundary>
-                {error && error.networkError && !error.networkError.response && (
-                    <p>
-                        Can't connect to the network.{' '}
-                        <button onClick={() => dispatch({type: 'refresh'})}>
-                            Try to re-connect
-                        </button>
-                    </p>
-                )}
-                {error && error.networkError && error.networkError.response && (
-                    <p>Network Error {error.networkError.statusCode}</p>
-                )}
-                {error && error.graphQLErrors && <p>Application Error (GraphQL).</p>}
-                <SearchSettings isLoading={isLoading} state={state} dispatch={dispatch} />
-                <SearchResults
-                    languages={state.languages}
-                    currentSearch={state.search.current}
-                    refreshCounter={state.refreshCounter}
-                    dispatch={dispatch}
-                />
-            </ErrorBoundary>
+            {error && error.networkError && !error.networkError.response && (
+                <p>
+                    Can't connect to the network.{' '}
+                    <button onClick={() => dispatch({type: 'refresh'})}>
+                        Try to re-connect
+                    </button>
+                </p>
+            )}
+            {error && error.networkError && error.networkError.response && (
+                <p>Network Error {error.networkError.statusCode}</p>
+            )}
+            {error && error.graphQLErrors && <p>Application Error (GraphQL).</p>}
+            <SearchSettings isLoading={isLoading} state={state} dispatch={dispatch} />
+            <SearchResults
+                languages={state.languages}
+                currentSearch={state.search.current}
+                refreshCounter={state.refreshCounter}
+                dispatch={dispatch}
+            />
         </>
     );
 }
