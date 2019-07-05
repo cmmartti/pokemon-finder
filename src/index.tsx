5d6f33b1bc21198246944708eb183a1e76f5f88a
Charles Marttinen <mail@charlesmarttinen.ca>
2019-07-05 17:18:29 -0400

    Update sanitize.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'sanitize.css';
+
 import 'focus-visible/dist/focus-visible.min.js';
+import 'sanitize.css';
+import 'sanitize.css/typography.css';
+import 'sanitize.css/forms.css';
 
 import './index.scss';
 import App from './components/App';
